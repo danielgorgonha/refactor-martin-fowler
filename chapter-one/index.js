01eeada733ed591e4e96d9af783407a312d95ce9
Daniel R Gorgonha <rogergorgonha@gmail.com>
2022-11-26 11:19:03 -0300

    refactor: aligning all functions

--- chapter-one/index.js
+++ chapter-one/index.js
@@ -1,6 +1,50 @@
 const invoices = require('./database/invoices.json');
 const plays = require('./database/plays.json');
 
+function statement(invoice, plays) {
+  let result = `Statement for ${invoice[0].customer}\n`;
+
+  for (let perf of invoice[0].performances) {
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+  }
+
+  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
+  
+  return result;  
+}
+
+function totalAmount() {
+  let result = 0;
+  for (let perf of invoices[0].performances) {
+    result += amountFor(perf);
+  }
+  return result;
+}
+
+function totalVolumeCredits() {
+  let result = 0;
+  for (let perf of invoices[0].performances) {
+    result += volumeCreditsFor(perf);
+  }
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber);
+}
+
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+  if ("comedy" === playFor(aPerformance).type)
+    result += Math.floor(aPerformance.audience / 5);
+  return result;
+}
 
 function playFor(aPerformance) {
   return plays[aPerformance.playID];
@@ -29,51 +73,5 @@ function amountFor(aPerformance) {
   
   return result;
 }
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ("comedy" === playFor(aPerformance).type)
-    result += Math.floor(aPerformance.audience / 5);
-  return result;
-}
-
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format(aNumber);
-}
-
-function totalVolumeCredits() {
-  let result = 0;
-  for (let perf of invoices[0].performances) {
-    result += volumeCreditsFor(perf);
-  }
-  return result;
-}
-
-function totalAmount() {
-  let result = 0;
-  for (let perf of invoices[0].performances) {
-    result += amountFor(perf);
-  }
-  return result;
-}
-
-function statement(invoice, plays) {
-  let result = `Statement for ${invoice[0].customer}\n`;
-
-  for (let perf of invoice[0].performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
-  }
-
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
-  
-  return result;  
-}
-
 
 console.log(statement(invoices, plays));
